2c833fcc8e3c40200786d23cdef58bc9e523f547
noob-prachurjo <prachurjobarmon2008@gmail.com>
2026-04-16 12:00:41 +0600

    full project initiated and navbar section completd

---
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import logo from "../../../img/logo.png"
+import { IoMdHome } from 'react-icons/io';
+import { FaHome } from 'react-icons/fa';
+import { CiStopwatch } from 'react-icons/ci';
+import { BsGraphUpArrow } from 'react-icons/bs';
+import Link from 'next/link';
+import Image from 'next/image';
+const Navbar = () => {
+  
+  return (
+    <nav className=" bg-base-100 shadow-sm">
+      <div className="container mx-auto navbar flex items-center justify-between">
+        <Image className="md:block hidden" src={logo} alt="logo"/>
+        <div className="">
+          <Link href="/" className='btn btn-ghost '><FaHome /> Home</Link>
+          <a href="/timeline" className='btn btn-ghost '><CiStopwatch /> Timeline</a>
+          <a href="/stats" className='btn btn-ghost'><BsGraphUpArrow /> Stats</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
